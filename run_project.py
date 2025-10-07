eb9526853860fba2a6ec5fd7b40bfc856a2a7204
Quinnan Gill <quinnan.gill@gmail.com>
2025-10-07 19:15:47 -0400

    Added run file

---
+++ run_project.py
@@ -0,0 +1,52 @@
+from argparse import ArgumentParser, Namespace
+from connect_four_env import ConnectFourEnv
+
+def run_monte_carlo(env: ConnectFourEnv, opts: Namespace):
+    # TODO: Implement this
+    pass
+
+def run_q_learning(env: ConnectFourEnv, opts: Namespace):
+    # TODO: Implement this
+    pass
+
+
+ALGO_MAP = {
+    "monte-carlo": run_monte_carlo,
+    "q-learning": run_q_learning,
+}
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("--algo", required=True, choices=list(ALGO_MAP.keys()))
+
+    parser.add_argument('-r', "--rows", type=int, default=3)
+    parser.add_argument('-c', "--columns", type=int, default=4)
+    parser.add_argument('-n', "--connect-n", type=int, default=3)
+
+    parser.add_argument("--reward", type=float, default=1.0)
+    parser.add_argument("--penaly", type=float, default=0.0)
+    parser.add_argument("--move-cost", type=float, default=0.0)
+
+    parser.add_argument("--gamma", type=float, default=0.8)
+    parser.add_argument("--alpha", type=float, default=1.0)
+    parser.add_argument("--epsilon", type=float, default=0.2)
+
+    parser.add_argument("--player", default="agent", choices=["agent", "random", "human"])
+
+    # if set don't save checkpoints (q-table)
+    # if set save the checkpoints as EPISODE_AGLO.bin
+    parser.add_argument('-o', "--checkpoint-dir", default="", type=str)
+
+    opts = parser.parse_args()
+
+    env = ConnectFourEnv(
+            opts.rows,
+            opts.cols,
+            opts.connect_n,
+            opts.reward,
+            opts.penalty,
+            opts.move_cost
+        )
+    
+    run_rl_algo = ALGO_MAP[opts.algo]
+    run_rl_algo(env, opts)
+    
